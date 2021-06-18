dca3aaa62b00171715ffb403d786cc2426738539
TatsuKawasaki <tatsuya-16@docomo.ne.jp>
2021-06-18 10:10:31 +0900

    add file C-HOS

---
+++ C-HOS/app/controllers/categories_controller.rb
@@ -0,0 +1,37 @@
+class CategoriesController < ApplicationController
+    def index
+        @category = Category.new
+        @categories = Category.all
+    end
+    
+    def create
+        @category = Category.new(category_params)
+        if @category.save
+            redirect_to categories_index_path
+        else
+            @categories = Category.all
+            render 'index'
+        end
+    end
+    
+    def edit
+        @category = Category.find(params[:id])
+    end
+    
+    def destroy
+        @category = Category.find(params[:id])
+        @category.destroy
+        redirect_to categories_index_path
+    end
+    
+    def update
+        @category = Category.find(params[:id])
+        @category.update(category_params)
+        redirect_to categories_index_path
+    end
+    
+private
+    def category_params
+        params.require(:category).permit(:category_name)
+    end
+end
