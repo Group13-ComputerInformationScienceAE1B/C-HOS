cf7bb80bc65c8f50f55cee0b267561d388de96c7
TsubasaWada <wadat2d@gmail.com>
2021-06-22 15:43:12 +0900

    first commit

--- C-HOS/config/routes.rb
+++ C-HOS/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :posts, only: [:new, :edit, :create, :update, :destroy]
+  root 'posts#index'
+  get 'posts' => 'posts#index'
+  get 'posts/new' => 'posts#new'
+  post 'posts' => 'posts#create'
+  get 'posts/:id' => 'posts#show'
+  post 'posts/:id/destroy' => 'posts#destroy'
 end
